596e04766b79f6b0ea5edd2358f2b4e33b6783f7
Judy Amster <judyamster@icloud.com>
2020-06-02 16:47:56 -0400

    diff address

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-const CLIENTURL = process.env.NODE_ENV === "production" ? "https://dev-client.now.sh" : "localhost:3000"
+const CLIENTURL = process.env.NODE_ENV === "production" ? "https://dev-client-8f5kvdedt.now.sh" : "localhost:3000"
 
 export default CLIENTURL
