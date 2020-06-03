c814657eb36facd57a8385a4e47cbccb280308cc
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-02 17:14:16 -0700

    fixed constants

--- constants.js
+++ constants.js
@@ -1,3 +1,6 @@
-const CLIENTURL = process.env.NODE_ENV === "production" ? "https://dev-client-8f5kvdedt.now.sh" : "localhost:3000"
+const CLIENTURL =
+  process.env.NODE_ENV === "production"
+    ? "https://dev-client-8f5kvdedt.now.sh"
+    : "http://localhost:3000";
 
-export default CLIENTURL
+export default CLIENTURL;
