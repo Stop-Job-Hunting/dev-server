ffe502caf9824a83305cbb228236ab38b0151e44
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-06 10:38:54 -0700

    removed slash at end of cors request header

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const CLIENTURL =
   process.env.NODE_ENV === "production"
-    ? "https://stopjobhunting.com/"
+    ? "https://stopjobhunting.com"
     : "http://localhost:3000";
 
 export default CLIENTURL;
