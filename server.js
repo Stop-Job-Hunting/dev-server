34bafafe671172fc17c916772daf1cf044334917
Judy Amster <judyamster@icloud.com>
2020-06-10 11:26:09 -0400

    added controllers to server

--- server.js
+++ server.js
@@ -24,9 +24,26 @@ database.connect();
 // Register our controllers here
 import SessionsController from "./controllers/SessionsController";
 import ResumesController from "./controllers/ResumesController";
+import AwardsController from "./controllers/AwardsController";
+import EducationsController from "./controllers/EducationsController";
+import InterestsController from "./controllers/InterestsController";
+import LanguagesController from "./controllers/LanguagesController";
+import PublicationsController from "./controllers/PublicationsController";
+import ReferencesController from "./controllers/ReferencesController";
+import SkillsController from "./controllers/SkillsController";
+import VolunteersController from "./controllers/VolunteersController";
 import WorksController from "./controllers/WorksController";
+
 app.use("/sessions", new SessionsController().router)
 app.use("/resumes", new ResumesController().router)
+app.use("/awards", new AwardsController().router)
+app.use("/educations", new EducationsController().router)
+app.use("/interests", new InterestsController().router)
+app.use("/languages", new LanguagesController().router)
+app.use("/publications", new PublicationsController().router)
+app.use("/references", new ReferencesController().router)
+app.use("/skills", new SkillsController().router)
+app.use("/volunteers", new VolunteersController().router)
 app.use("/works", new WorksController().router)
 
 app.get("/", (req, res) => {
