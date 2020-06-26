29bfd282799cc425232288c0fd6e2baea959d7c0
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-26 12:39:33 -0700

    pulls data from DB and awaits the promises and then prepares it to be parsed into a fresh schema

--- controllers/DownloadController.js
+++ controllers/DownloadController.js
@@ -1,42 +1,37 @@
-import { dbContext } from "../db/DbContext"
-import express from "express"
-import { validationService } from "../service/ValidationService"
-
+import { dbContext } from "../db/DbContext";
+import express from "express";
+import { validationService } from "../service/ValidationService";
 
 export default class DownloadController {
   constructor() {
-    this.router = express
-      .Router()
-      .get("", this.getAll)
+    this.router = express.Router().get("", this.getAll);
   }
 
   async getAll(req, res, next) {
-
+    console.log("triggered request");
     // Their particular username to store with the data
     let username = await validationService.validateUser(req);
-    if (username === "") return (res.status(401))
+    if (username === "") return res.status(401);
 
     // TODO:  if validateUser returns empty username, what to do
 
-    let allItems = []
-    dbContext.Basic.find({ username: username }, function (err, documents) {
-      if (err) throw console.error(err);
-      allItems.push(documents)
-    });
+    let basicItems = await dbContext.Basic.find({ username: username }).exec();
 
-    dbContext.Work.find({ username: username }, function (err, documents) {
-      if (err) throw console.error(err);
-      allItems.push(documents)
-    });
+    let workItems = await dbContext.Work.find({ username: username }).exec();
 
-    dbContext.Education.find({ username: username }, function (err, documents) {
-      if (err) throw console.error(err);
-      allItems.push(documents)
-    });
+    let educationItems = await dbContext.Education.find({
+      username: username,
+    }).exec();
 
-    return res.send(allItems)
+    let allItems = {
+      basics: basicItems[0],
+      work: workItems,
+      education: educationItems,
+    };
+
+    console.log("combined docs: ", allItems);
+
+    return res.send(allItems);
     res.status(200);
-
   }
-
 }
